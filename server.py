d64e63123086f319f01e05c529fc9b5b99134294
Mattia Compagnone <mattcompa@icloud.com>
2024-07-08 16:42:19 +0200

    Fifth Commit

--- server.py
+++ server.py
@@ -1,80 +1,85 @@
 import os
 from urllib.parse import urlencode, quote_plus
 
-from flask import Flask, render_template, url_for, session, abort, redirect
+from flask import Flask, url_for, session, abort, redirect, request, jsonify
 from authlib.integrations.flask_client import OAuth
-import json
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
-
+app.secret_key = os.getenv("SECRET_KEY")
 appConf = {
-    "OAUTH2_CLIENT_ID": os.getenv("OAUTH2_CLIENT_ID"),
-    "OAUTH2_CLIENT_SECRET": os.getenv("OAUTH2_CLIENT_SECRET"),
-    "OAUTH2_ISSUER": os.getenv("OAUTH2_ISSUER"),
-    "FLASK_SECRET": os.getenv("FLASK_SECRET"),
-    "FLASK_PORT": os.getenv("FLASK_PORT"),
+    "CLIENT_ID": os.getenv("CLIENT_ID"),
+    "CLIENT_SECRET": os.getenv("CLIENT_SECRET"),
+    "URL": os.getenv("URL"),
+    "KEYCLOAK_ADMIN": os.getenv("KEYCLOAK_ADMIN"),
+    "KEYCLOAK_ADMIN_PASSWORD": os.getenv("KEYCLOAK_ADMIN_PASSWORD"),
+    "REALM_NAME": os.getenv("REALM_NAME"),
+    "REDIRECT_URI": os.getenv("REDIRECT_URI"),
+    "LOGOUT_REDIRECT_URI": os.getenv("LOGOUT_REDIRECT_URI")
 }
-
-
-app.secret_key = appConf["FLASK_SECRET"]
-oauth = OAuth(app)
-oauth.register(
-    name="myApp",
-    client_id=appConf.get("OAUTH2_CLIENT_ID"),
-    client_secret=appConf.get("OAUTH2_CLIENT_SECRET"),
-    client_kwargs={
-        "scope": "openid email profile",
-        "code_challenge_method": "S256", #enable PKCE
-    },
-    server_metadata_url=f'{appConf.get("OAUTH2_ISSUER")}/.well-known/openid-configuration',
-)
-
-
-@app.route("/")
-def home():
-    return render_template("home.html", session=session.get("user"), pretty=json.dumps(session.get("user"), indent=4))
-
 
 @app.route("/callback")
 def callback():
-    token = oauth.myApp.authorize_access_token()
-    session["user"] = token
-    return redirect(url_for("home"))
+    code = request.args.get("code")
+    if code is None:
+        abort(400)
+    token_url = f'{appConf.get("URL")}/protocol/openid-connect/token'
+    data = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "client_id": appConf.get("CLIENT_ID"),
+        "client_secret": appConf.get("CLIENT_SECRET"),
+        "redirect_uri": f"{appConf.get('REDIRECT_URI')}",
+    }
+    response = requests.post(token_url, data=data)
+
+    if response.status_code == 200:
+        token_response = response.json()
+        session["user"] = token_response.get("id_token")
+        session["access_token"] = token_response.get("access_token")
+        return jsonify(token_response)
+    else:
+        return jsonify({"error": "Can't retrieve token"}), response.status_code
 
 
 @app.route("/login")
 def login():
     if "user" in session:
         abort(404)
-    return oauth.myApp.authorize_redirect(redirect_uri=url_for('callback', _external=True))
+    auth_url = f'{appConf.get("URL")}/protocol/openid-connect/auth'
+    params = {
+        'client_id': appConf.get("CLIENT_ID"),
+        'response_type': 'code',
+        'scope': 'openid',
+        'redirect_uri': appConf.get("REDIRECT_URI"),
+    }
+    return redirect(f"{auth_url}?client_id={params['client_id']}&response_type={params['response_type']}&scope={params['scope']}&redirect_uri={params['redirect_uri']}")
 
 
-@app.route("/loggedout")
-def loggedout():
-    if "user" in session:
-        abort(404)
-    return redirect(url_for('home'))
+@app.route("/token", methods=['POST'])
+def token():
+    username = request.json.get("username")
+    password = request.json.get("password")
+    token_url = f'{appConf.get("URL")}/protocol/openid-connect/token'
+    data = {
+        "grant_type": "password",
+        "username": username,
+        "password": password,
+        "client_id": appConf.get("CLIENT_ID"),
+        "client_secret": appConf.get("CLIENT_SECRET"),
+    }
+    response = requests.post(token_url, data=data)
+    return jsonify(response.json())
 
 
 @app.route("/logout")
 def logout():
-    id_token = session["user"]["id_token"]
     session.clear()
-    return redirect(
-        appConf.get("OAUTH2_ISSUER")
-        + "/protocol/openid-connect/logout?"
-        + urlencode(
-            {
-                "post_logout_redirect_uri": url_for('loggedout', _external=True),
-                "id_token_hint": id_token
-            },
-            quote_via=quote_plus
-        )
-    )
+    return redirect('/login')
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(debug=True)
